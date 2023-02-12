3b174014fbb975a0a6396634f9e590ca908b02b3
Sudipto-das <37843943+Sudipto-das@users.noreply.github.com>
2023-02-12 17:51:35 +0000

    change code

--- todoList/index.js
+++ todoList/index.js
@@ -1,30 +1,58 @@
 const form = document.querySelector("form");
-const input = document.querySelector("#task");
+// const input = document.querySelector("#task");
 const todoList = document.querySelector("#todolist");
+let tasks=[]
 
-form.addEventListener("submit", addTask);
+// form.addEventListener("submit", addTask);
+// Define a Task class to represent each task
+class Task {
+  constructor(name, startDate, endDate) {
+    this.name = name;
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
+   // Method to display the task information
+   displayTask() {
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("task-item");
+    taskItem.innerHTML = `
+      <div>${this.name}</div>
+      <div>Start Date: ${this.startDate}</div>
+      <div>End Date: ${this.endDate}</div>
+    `;
+    todoList.appendChild(taskItem);
+    console.log("task added")
+  }
+}
+function addTask(name, startDate, endDate){
+  const task = new Task(name, startDate, endDate);
+  tasks.push(task);
+  task.displayTask
+}
 
-// adding task
+  
 
-function addTask(e) {
+form.addEventListener("submit", e => {
   e.preventDefault();
-  const task = input.value;
-  if (!task) return;
-  const li = document.createElement("li");
-  li.innerHTML = task;
-  todoList.appendChild(li);
+  const taskName = document.querySelector("#task").value;
+  const startDate = document.querySelector("#startdate").value;
+  const endDate = document.querySelector("#enddate").value;
+  if (taskName && startDate && endDate) {
+    addTask(taskName, startDate, endDate);
+  }
+});
 
 // Deleting task
 
-  li.textContent = task;
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Delete";
+  // li.textContent = task;
+  // const deleteButton = document.createElement("button");
+  // deleteButton.textContent = "Delete";
 
-  deleteButton.addEventListener("click", function () {
-    li.remove();
-  });
+  // deleteButton.addEventListener("click", function () {
+  //   li.remove();
+  // });
 
-  li.appendChild(deleteButton);
-  todoList.appendChild(li);
-  input.value = "";
-}
+  // li.appendChild(deleteButton);
+  // todoList.appendChild(li);
+  // input.value = "";
+
