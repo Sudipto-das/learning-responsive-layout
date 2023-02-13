f824a2bd46dbe55a3dada2ed30451b1f9146420d
Sudipto-das <37843943+Sudipto-das@users.noreply.github.com>
2023-02-13 03:19:46 +0000

    add delete button

--- todoList/index.js
+++ todoList/index.js
@@ -1,34 +1,4 @@
-// const form = document.querySelector("form");
-// // const input = document.querySelector("#task");
-// const todoList = document.querySelector("#todolist");
-// let tasks=[]
 
-// // form.addEventListener("submit", addTask);
-// // Define a Task class to represent each task
-// class Task {
-//   constructor(name, startDate, endDate) {
-//     this.name = name;
-//     this.startDate = startDate;
-//     this.endDate = endDate;
-//   }
-//    // Method to display the task information
-//    displayTask() {
-//     const taskItem = document.createElement("div");
-//     taskItem.classList.add("task-item");
-//     taskItem.innerHTML = `
-//       <div>${this.name}</div>
-//       <div>Start Date: ${this.startDate}</div>
-//       <div>End Date: ${this.endDate}</div>
-//     `;
-//     todoList.appendChild(taskItem);
-    
-//   }
-// }
-// function addTask(name, startDate, endDate){
-//   const task = new Task(name, startDate, endDate);
-//   tasks.push(task);
-//   task.displayTask
-// }
 
 const taskForm = document.querySelector(".task-form");
 const taskList = document.querySelector("#task-list");
@@ -73,15 +43,10 @@ taskForm.addEventListener("submit", e => {
 });
   
 
-// form.addEventListener("submit", e => {
-//   e.preventDefault();
-//   const taskName = document.querySelector("#task").value;
-//   const startDate = document.querySelector("#startdate").value;
-//   const endDate = document.querySelector("#enddate").value;
-//   if (taskName && startDate && endDate) {
-//     addTask(taskName, startDate, endDate);
-//   }
-// });
+const deletebutton = document.querySelector("#delete")
+deletebutton.addEventListener("click",function(){
+  taskList.remove()
+})
 
 // Deleting task
 
